74e475d2cb9b61df16938eebfe3580b4689cedbe
sikatanju <saadtanju@gmail.com>
2025-04-10 06:50:10 +0600

    add django debug toolbar

--- biztrack/settings.py
+++ biztrack/settings.py
@@ -38,13 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "debug_toolbar",
     'rest_framework',
     'djoser',
+
     'core',
     'biztrack_app',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -73,6 +77,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'biztrack.wsgi.application'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Database
